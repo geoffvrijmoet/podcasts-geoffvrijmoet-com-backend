095c382191cd253da104b4564cb2d26cc3383a2b
Geoff Vrijmoet <geofferyv@gmail.com>
2025-01-07 19:31:18 -0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/',
-          has: [
-            {
-              type: 'host',
-              value: 'pay.podcasts.geoffvrijmoet.com',
-            },
-          ],
-          destination: '/pay',
-        },
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'pay.podcasts.geoffvrijmoet.com',
-            },
-          ],
-          destination: '/pay/:path*',
-        },
-      ],
-    };
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pay.podcasts.geoffvrijmoet.com',
+          },
+        ],
+        destination: '/pay/:path*',
+      },
+    ];
   },
 };
 
